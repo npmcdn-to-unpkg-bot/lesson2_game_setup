ea301c2eb29421ffadea305e4bd8f22f3c5588a9
LebedyukIgor <ilebeduyk@gmail.com>
2016-05-27 11:36:30 +0300

    done

---
+++ app/app.components.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import {PuzzleComponent} from "./puzzle/puzzle.component";
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <my-puzzle></my-puzzle>
+        `,
+    directives: [PuzzleComponent]
+})
+export class AppComponent{}
